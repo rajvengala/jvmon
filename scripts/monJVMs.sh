cf4a644b50884db6ba9c41e1f743cc02c73fac3a
uglyhunK <mail@uglyhunk.in>
2011-07-22 16:19:34 +0530

    modified linux shell scripts and .gitignore

--- scripts/monJVMs.sh
+++ scripts/monJVMs.sh
@@ -3,6 +3,6 @@ JDK_HOME=/usr/local/java/jdk1.6.0_26
 PATH=$JDK_HOME/bin
 
 # Monitor all JVMs with description oracle.oats and jconsole every 15 seconds for an hour while scanning for new JVMs every minute
-java -classpath .:jvmon.jar:$JDK_HOME/lib/tools.jar in.uglyhunk.jvm.mon.Main f 15 240 jconsole,oracle.oats 60
+java -classpath .:../dist/jvmon.jar:$JDK_HOME/lib/tools.jar in.uglyhunk.jvm.mon.Main b 5 20 netbeans 60
 
 
