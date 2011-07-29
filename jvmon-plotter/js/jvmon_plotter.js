151eaa98a0b4ad26b647e9c8139d0ad4fb3e2113
uglyhunK <mail@uglyhunk.in>
2011-07-29 12:46:54 +0530

    updated jvmon-plotter.js x-axis label width

--- jvmon-plotter/js/jvmon_plotter.js
+++ jvmon-plotter/js/jvmon_plotter.js
@@ -3,93 +3,98 @@ var gProcId;
 var chartFuncNames;
 	
 function plotGraph(procId){	
-	gProcId = procId;
-	var procIdDisplayName = procId.substring(1);
-	var chartDivId = ["memory", "classes", "threads", "compilation", "gc", "memPool"];
-	chartFuncNames = ["Mem", "Class", "Thrd", "Comp", "GC", "MemPool"];
-	var legendLabels = ["memoryLegend", "classesLegend", "threadsLegend", "compilationLegend", "gcLegend", "memPoolLegend"];
-	var titles = [procIdDisplayName + " - Memory", procIdDisplayName + " - Classes", procIdDisplayName + " - Threads", procIdDisplayName + 
-					" - Compilation", procIdDisplayName + " - Garbage Collectors", procIdDisplayName + " - Memory Pool"];
+    gProcId = procId;
+    var procIdDisplayName = procId.substring(1);
+    var chartDivId = ["memory", "classes", "threads", "compilation", "gc", "memPool"];
+    chartFuncNames = ["Mem", "Class", "Thrd", "Comp", "GC", "MemPool"];
+    var legendLabels = ["memoryLegend", "classesLegend", "threadsLegend", "compilationLegend", "gcLegend", "memPoolLegend"];
+    var titles = [procIdDisplayName + " - Memory", procIdDisplayName + " - Classes", procIdDisplayName + 
+                " - Threads", procIdDisplayName + " - Compilation", procIdDisplayName + " - Garbage Collectors", 
+                procIdDisplayName + " - Memory Pool"];
 	
-	for(var index in chartDivId){
-		if(index !=4) {
-			g = new Dygraph(
-					document.getElementById(chartDivId[index]),
-					eval(procId + chartFuncNames[index]),
-					{
-						"title" : titles[index],
+    for(var index in chartDivId){
+        if(index !=4) {
+            g = new Dygraph(
+                document.getElementById(chartDivId[index]),
+                eval(procId + chartFuncNames[index]),
+                {
+                    "title" : titles[index],
 
-						"xlabel" : "Time",
-						"xAxisLabelWidth" : 60,
+                    "xlabel" : "Time",
+                    "xAxisLabelWidth" : 65,
 				
-						"legend" : "always",
+                    "legend" : "always",
 
-						"strokeWidth" : 3,
-						"fillGraph" : false,
-						"highlightCircleSize" : 3,
+                    "strokeWidth" : 3,
+                    "fillGraph" : false,
+                    "highlightCircleSize" : 3,
 
-						"labelsDiv": legendLabels[index],
-						"labelsSeparateLines" : true,
+                    "labelsDiv": legendLabels[index],
+                    "labelsSeparateLines" : true,
 
-						"rollPeriod": 1,
-						"showRoller": true
-					}
-				);
-		}
-	}
+                    "rollPeriod": 1,
+                    "showRoller": true
+                }
+                );
+        }
+    }
 	
-	// draw Garbage Collector Graph
-	gcGraph = new Dygraph(
-			document.getElementById(chartDivId[4]),
-			eval(procId + chartFuncNames[4]),
-			{
-				"title" : titles[4],
+    // draw Garbage Collector Graph
+    gcGraph = new Dygraph(
+        document.getElementById(chartDivId[4]),
+        eval(procId + chartFuncNames[4]),
+        {
+            "title" : titles[4],
 
-				"xlabel" : "Time",
-				"xAxisLabelWidth" : 60,
+            "xlabel" : "Time",
+            "xAxisLabelWidth" : 60,
 				
-				"visibility" : eval(gcColumnChooser(false, true)),
+            "visibility" : eval(gcColumnChooser(false, true)),
 
-				"legend" : "always",
+            "legend" : "always",
 
-				"strokeWidth" : 3,
-				"fillGraph" : false,
-				"highlightCircleSize" : 3,
+            "strokeWidth" : 3,
+            "fillGraph" : false,
+            "highlightCircleSize" : 3,
 
-				"labelsDiv": legendLabels[4],
-				"labelsSeparateLines" : true,
+            "labelsDiv": legendLabels[4],
+            "labelsSeparateLines" : true,
 
-				"rollPeriod": 1,
-				"showRoller": true
-			}
-		);
-	document.getElementById("gcToggle").innerHTML="Toggle GC Time and Count<input type=\"checkbox\" name=\"gcToggle\" style=\"margin:2px;\" " + 
-													"onclick=\"updateGCGraph(this.checked);\">";
+            "rollPeriod": 1,
+            "showRoller": true
+        }
+        );
+    document.getElementById("gcToggle").innerHTML="Toggle GC Time and Count<input type=\"checkbox\" name=\"gcToggle\" style=\"margin:2px;\" " + 
+"onclick=\"updateGCGraph(this.checked);\">";
 }	
 
 function gcColumnChooser(col1, col2){
-	var gcColumnsLine = eval(gProcId + chartFuncNames[4] + "()").split("\n")[0];
-	var gcColumns = gcColumnsLine.split(",");
-	var gcColumnVisibility = "["
-	for(var index in gcColumns){
-		if(index > 0)
-			gcColumnVisibility += ",";
-		if(index%2 == 0)
-			gcColumnVisibility += col1;
-		else
-			gcColumnVisibility += col2;
-	}
-	return gcColumnVisibility += "]";
+    var gcColumnsLine = eval(gProcId + chartFuncNames[4] + "()").split("\n")[0];
+    var gcColumns = gcColumnsLine.split(",");
+    var gcColumnVisibility = "["
+    for(var index in gcColumns){
+        if(index > 0)
+            gcColumnVisibility += ",";
+        if(index%2 == 0)
+            gcColumnVisibility += col1;
+        else
+            gcColumnVisibility += col2;
+    }
+    return gcColumnVisibility += "]";
 }
 			
 function updateGCGraph(checkboxStatus){
-	if(checkboxStatus){
-		gcGraph.updateOptions(
-								{ "visibility" : eval(gcColumnChooser(true, false)) }
-							);
-	} else {
-		gcGraph.updateOptions(
-								{"visibility" : eval(gcColumnChooser(false, true))}
-							);
-	}
+    if(checkboxStatus){
+        gcGraph.updateOptions(
+        {
+            "visibility" : eval(gcColumnChooser(true, false))
+        }
+        );
+    } else {
+        gcGraph.updateOptions(
+        {
+            "visibility" : eval(gcColumnChooser(false, true))
+            }
+        );
+    }
 }
